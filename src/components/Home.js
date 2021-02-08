8dfb7becd8f09dc1d56dbeaa1592c5935a6b0e8d
marina <nicolaimarina@gmail.com>
2021-02-08 16:07:03 -0500

    fetching the data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import {
+    API_URL,
+    API_KEY,
+    API_BASE_URL,
+    POSTER_SIZE,
+    BACKDROP_SIZE
+} from '../config';
 
 import HeroImage from './elements/HeroImage';
 import SearchBar from './elements/SearchBar';
@@ -7,15 +15,49 @@ import MovieThumb from './elements/MovieThumb';
 import LoadMoreBtn from './elements/LoadMoreBtn';
 import Spinner from './elements/Spinner';
 
-const Home = () => (
-    <>
-        <HeroImage />
-        <SearchBar />
-        <Grid />
-        <MovieThumb />
-        <Spinner />
-        <LoadMoreBtn />
-    </>
-)
+const Home = () => {
+    const [state, setState] = useState({ movies: [] })
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    console.log(state);
+
+    const fetchMovies = async endpoint => {
+        setError(false);
+        setLoading(true);
+
+        try {
+            const result = await (await fetch(endpoint)).json();
+            setState(prev => ({
+                ...prev,
+                movies: [...result.results],
+                heroImage: prev.heroImage  result.results[0],
+                currentPage: result.page,
+                totalPages: result.total_pages,
+
+            }));
+        } catch (error) {
+            setError(true);
+            console.log(error);
+        }
+        setLoading(false);
+
+    }
+
+    useEffect(() => {
+        fetchMovies(`${API_URL}movie/popular?api=${API_KEY}`);
+    })
+
+    return (
+        <>
+            <HeroImage />
+            <SearchBar />
+            <Grid />
+            <MovieThumb />
+            <Spinner />
+            <LoadMoreBtn />
+        </>
+    )
+}
 
 export default Home;
