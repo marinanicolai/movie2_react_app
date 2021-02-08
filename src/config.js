388f7da319d06def810c525cdd15261b54c56291
marina <nicolaimarina@gmail.com>
2021-02-08 15:40:14 -0500

    update

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 // Read more about the API here: https://developers.themoviedb.org/
 
 const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
+const API_KEY = '6dd38c5f-482c-4b76-bb91-7c28b79d1904';
 const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
 // Sizes: w300, w780, w1280, original
 const BACKDROP_SIZE = 'w1280';
